8d77ad4c2f4abb26e4e9df4085ec4b6de5530ed7
tenhoukaku <79339753+tenhoukaku@users.noreply.github.com>
2026-05-30 11:17:56 -0300

    Revert

--- lutz-save/script.js
+++ lutz-save/script.js
@@ -4,11 +4,11 @@ convertButton.addEventListener("click", (e) => {
 	val = convertText.value.trim()
 	console.log(val)
 	if (!val) return
-	if (val.startsWith("CklfoB")) {
+	if (val.startsWith("Cklfoa")) {
 		val = atob(val.replace(/Cklfoa/,"")).replace(/\{|\}/g,"").split(",").join("\n")
 	}
 	else {
-		val = "CklfoB" + btoa("{" + val.split("\n").filter(n => n).join(",") + "}")
+		val = "Cklfoa" + btoa("{" + val.split("\n").filter(n => n).join(",") + "}")
 	}
 	convertText.value = val
 })
